07fa1b2013d8e090f7630bf3cfbfdc17ece26fe8
mariaangela <136354649+angelalala01@users.noreply.github.com>
2024-03-03 16:16:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const openBtn = document.getElementById('openBtn');
+const closeBtn = document.getElementById('closeBtn');
+const popup = document.getElementById('popup');
+
+openBtn.addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the default behavior of the anchor tag
+  popup.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', function() {
+  popup.style.display = 'none';
+});
+
+$(function() {
+  var $mybook 		= $('#mybook');
+  var $bttn_next		= $('#next_page_button');
+  var $bttn_prev		= $('#prev_page_button');
+  var $loading		= $('#loading');
+  var $mybook_images	= $mybook.find('img');
+  var cnt_images		= $mybook_images.length;
+  var loaded			= 0;
+   
+  $mybook_images.each(function(){
+    var $img 	= $(this);
+    var source	= $img.attr('src');
+    $('<img/>').load(function(){
+      ++loaded;
+      if(loaded == cnt_images){
+        $loading.hide();
+        $bttn_next.show();
+        $bttn_prev.show();
+        $mybook.show().booklet({
+          name:               null,                            //  
+          width:              800,                             //  
+          height:             500,                             //   
+          speed:              600,                             //  
+          direction:          'LTR',                           //  
+                                     //  
+          next:               $bttn_next,          			//  
+          prev:               $bttn_prev,          			//  
+                                 
+        });
+        Cufon.refresh();
+      }
+    }).attr('src',source);
+  });
+  
+});
